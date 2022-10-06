95485e6a270d42681c575620c5d09214a0a98f98
foysal767 <uddinmohammadfoysal981@gmail.com>
2022-10-07 00:28:46 +0600

    cart component added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({cart}) => {
     return (
-        <div>
+        <div className='cart'>
             <h4>Order summary</h4>
             <h4>Selected Items = {cart.length}</h4>
         </div>
